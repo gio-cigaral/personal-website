f050264765bf70ffb309d14812f029d2a9fc4850
Gianpaolo Cigaral <gio.cigaral@gmail.com>
2021-07-01 16:25:30 +1200

    add content to technology page

--- client/src/components/technology.js
+++ client/src/components/technology.js
@@ -11,7 +11,34 @@ function Technology() {
       </header>
 
       <main className="technology-main page-main">
-        <h2>Hello World</h2>
+        <article>
+          <header>Languages</header>
+          <ul>
+            <li>Java</li>
+            <li>C++</li>
+            <li>Python</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+          </ul>
+        </article>
+        <article>
+          <header>Frameworks</header>
+          <ul>
+            <li>React</li>
+          </ul>
+        </article>
+        <article>
+          <header>Tools</header>
+          <ul>
+            <li>Android</li>
+            <li>Android Studio</li>
+            <li>Node</li>
+            <li>Visual Studio Code</li>
+            <li>IntelliJ</li>
+            <li>PyCharm</li>
+          </ul>
+        </article>
       </main>
     </div>
   );
